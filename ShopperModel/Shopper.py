67a3e14c3aa9e7c01ce9795ea2010e0bb1586a48
cmutuc <mutuc.c@northeastern.edu>
2020-06-22 21:16:12 -0400

    added documentation to shopper table

--- ShopperModel/Shopper.py
+++ ShopperModel/Shopper.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
 
+
 class Shopper:
     """
     A Shopper that is visiting the store. The constructor takes in the Day the shopper is visiting
@@ -10,9 +11,6 @@ class Shopper:
     normalmintime = 6
     normalmaxtime = 75
     normalavgtime = 25
-    # For Carlo:
-    # lunch_start and end and dinner_start and end in Day class
-    # And lunch_avg_time_spent, dinner_avg_time_spent, weekend_increase, default_senior, default_sunny
 
     def __init__(self, day, time_in, is_sunny, percent_senior):
         self.date = day.date
@@ -21,11 +19,16 @@ class Shopper:
         self.is_sunny = is_sunny
         self.time_spent = self.__generate_time_spent()
         self.percent_senior = percent_senior
-        self.is_senior = np.random.choice(a=np.array([True, False]), p=[self.percent_senior, 1 - self.percent_senior])
+        self.is_senior = np.random.choice(a=np.array([True, False]),
+                                          p=[self.percent_senior, 1 - self.percent_senior])
 
     def __generate_time_spent(self):
         return round(random.triangular(self.normalmintime, self.normalmaxtime, self.normalavgtime))
 
     def shopper_parameters_to_dictionary(self):
-        return {'Date': self.date, "DayOfWeek": self.day_of_week, "TimeIn": self.time_in, "TimeSpent": self.time_spent,
-                "IsSenior": self.is_senior, "IsSunny": self.is_sunny}
+        """
+        Returns this shoppers and its variables as a dictionary
+        :return: dictionary with the wanted variables for a shopper
+        """
+        return {'Date': self.date, "DayOfWeek": self.day_of_week, "TimeIn": self.time_in,
+                "TimeSpent": self.time_spent, "IsSenior": self.is_senior, "IsSunny": self.is_sunny}
--- ShopperModel/ShopperTable.py
+++ ShopperModel/ShopperTable.py
@@ -2,6 +2,10 @@ import pandas as pd
 
 
 class ShopperTable:
+    """
+    This class contains the objects necessary to create a shopper table and a
+    method to create the shopper data
+    """
 
     def __init__(self, store_model, time_frame):
         self.store_model = store_model
@@ -9,12 +13,18 @@ class ShopperTable:
         self.data_frame = None
 
     def create_table(self):
+        """
+        Populates this class with a data frame of the shoppers generated based
+        on the passed in parameters
+        :return: None
+        """
         day_list = []
         # create all the days
         for date in self.time_frame.dates:
             day_list.append(self.store_model.create_day(date))
 
-        shopper_dict = {'Date': [], 'DayOfWeek': [], 'TimeIn': [], 'TimeSpent': [], 'IsSenior': [], 'IsSunny': []}
+        shopper_dict = {'Date': [], 'DayOfWeek': [], 'TimeIn': [], 'TimeSpent': [],
+                        'IsSenior': [], 'IsSunny': []}
 
         for day in day_list:
             day.create_shoppers()
@@ -29,8 +39,12 @@ class ShopperTable:
         return self.data_frame
 
     def to_csv(self, path):
+        """
+        Generates a csv of the shoppers given file path
+        :param path: a filepath to save the generated shopper data
+        :return: None
+        """
         if self.data_frame is None:
             self.create_table()
 
         return self.data_frame.to_csv(path)
-
