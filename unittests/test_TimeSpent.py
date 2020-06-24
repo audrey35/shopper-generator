e72bd0342109fe171ae5fe215b489c854dfd4cbf
simpsone <simpson.ev@husky.neu.edu>
2020-06-23 18:27:07 -0700

    temp

---
+++ unittests/test_TimeSpent.py
@@ -0,0 +1,9 @@
+import unittest
+from Configuration.store_model import StoreModel
+import ShopperModel.shopper
+
+class TestTimeSpent(unittest.TestCase):
+    def test_time_spent(self):
+        store_model =
+ def __init__(self, lunch_rush, dinner_rush, holiday_modifiers, sunny_modifiers, senior_discount,
+                 open_time='06:00', close_time='21:00', percent_senior=0.2):
